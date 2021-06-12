34138920e30a72b87a1aefa6fe7bcf2b1f8df0fc
Matt Kragen <mattdkragen@gmail.com>
2021-06-12 12:21:51 -0700

    Put attr accessor into Person stoned attribute

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -9,12 +9,13 @@ class Medusa
   
   def stare(victim)
     @statues << victim
-    victim.
+    victim.stoned = true
   end
 end
 
 class Person
   attr_reader :name
+  attr_accessor :stoned
 
   def initialize(name)
     @name = name
