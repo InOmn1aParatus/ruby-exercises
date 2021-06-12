b04b1f8ac892bf4839a5de9c07a85e798799990c
Matt Kragen <mattdkragen@gmail.com>
2021-06-12 12:20:03 -0700

    Move Person into Medusa; delete Person spec

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -6,4 +6,22 @@ class Medusa
     @name = name
     @statues = []
   end
+  
+  def stare(victim)
+    @statues << victim
+    victim.
+  end
+end
+
+class Person
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @stoned = false
+  end
+
+  def stoned?
+    @stoned
+  end
 end
