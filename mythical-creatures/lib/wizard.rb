bd4a027a1ec2fdb90314c920f18e78394b853f73
Matt Kragen <mattdkragen@gmail.com>
2021-06-12 09:43:57 -0700

    Complete Wizard

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -1,7 +1,32 @@
 class Wizard
-  attr_reader :name
+  attr_reader :name,
+              :bearded,
+              :rested
 
-  def initiallize(name)
+  def initialize(name, bearded: true)
     @name = name
+    @bearded = bearded
+    @rested = 0
+  end
+
+  def bearded?
+    @bearded
+  end
+
+  def incantation(phrase)
+    "sudo #{phrase}"
+  end
+
+  def rested?
+    if @rested < 3
+      true
+    else
+      false
+    end
+  end
+
+  def cast
+    @rested += 1
+    'MAGIC MISSILE!'
   end
 end
